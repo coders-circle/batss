eeb4834a883f15dbbd99c0c9187688beb4464735
bibek <bibekdahal.bd16@gmail.com>
2015-09-01 18:13:36 +0545

    Implement Backpropagation Learning Algorithm

---
+++ batss/backprop/helpers.py
@@ -0,0 +1,23 @@
+import random
+import numpy as np
+
+
+def activate(value):
+    """Bipolar Sigmoid activation function"""
+
+    t = 2.0 / (1+np.exp(-value)) - 1
+    if t >= 1:
+        print(value)
+    return t
+
+
+def activate_diff(value):
+    """Derivative of sigmoid activation function"""
+
+    f = activate(value)
+    return 0.5 * (1+f) * (1-f)
+
+
+def get_random_weight(div_factor=1):
+    """Get a random value in range (-.5, .5) divided by given factor"""
+    return (random.random() - 0.5) / (div_factor)
